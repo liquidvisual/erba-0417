21360a5dddb4db362aa5e27e24c485e1d97ab4fa
Mitchell Warren <hello@liquidvisual.com>
2017-05-09 13:14:06 +1000

    Gallery + popup

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,7 +14,33 @@ $(function() {
     $(window).on('load', function() {
         $('html').addClass('has-loaded');
         $('input, textarea').placeholder(); // IE9 Patch
+        // $('[data-masonry]').masonry();
+        lightbox();
     });
+
+    //-----------------------------------------------------------------
+    // POPUP GALLERY
+    //-----------------------------------------------------------------
+
+    function lightbox() {
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small></small>';
+                }
+            }
+        });
+    }
 
     //-----------------------------------------------------------------
     // LAUNCH MANAGE ON KEYPRESS

03b0519e1a5fff946c52ee471cebf436d194789a
liquidvisual <liquidvisual@gmail.com>
2019-06-18 12:47:48 +1000

    Erbas asset updates for Microsite

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,12 +11,54 @@ $(window).on('load', function(event) {
 });
 
 //-----------------------------------------------------------------
-// DOCUMENT READY
+// ADHOC THEME SWITCHING BETWEEN MICROSITE
 //-----------------------------------------------------------------
 
-// $(function() {
+if (window.location.hostname === 'erbas.yourwebvisual.com' ||
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '10.0.1.3') {
 
-// });
+    var logo = document.querySelectorAll('.global-header .logo');
+
+    // set temporary local storage variable - continue theme across pages
+    if (!!localStorage.getItem('theme-sustain')) {
+        setThemeSustain();
+    }
+
+    // logo toggle
+    logo.forEach(function(item) {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var isDefault = this.classList.contains('is-default');
+            var isSustain = this.classList.contains('is-sustain');
+
+            // determine logo
+            if (isDefault) unsetThemeSustain();
+            if (isSustain) setThemeSustain();
+        })
+    });
+
+    function setThemeSustain() {
+        document.body.classList.add('theme-sustain');
+        localStorage.setItem('theme-sustain', true);
+        document.querySelector('.lv-hero > .lv-hero-item:first-child').style.backgroundImage = 'url(/assets/img/content/hero-sustain.jpg)';
+        document.querySelector('.lv-hero-caption .panel-feature h1').innerHTML = 'Environmentally Sustainable Design';
+
+        document.querySelector('.lv-hero-caption .panel-feature p').innerHTML = "Vndip samus eum que voloru mqui conserore videlis nusciis cidunt harum et et quam et adipsum corum est enihilitis autam renihil ipsunt laut officiis nosaess eculpa seque seruptatur, im ilit quo es as eicit, tempori animpe vident porest quibust voluptatquia dolorupici que nus, ersperi tatiandae. Nam et qui nonserunti am harum rem eosa doloria tempore dolorro videlis moluptate sequi volorem poreper ionsequ assedit, quod ullaboraerio odigend itatur re cores dita quatusciti omnist excearum hit quo qui conem quae pa nimo qui omniend emoluptas ilis sumquis re apienis exped qua.";
+    }
+
+    function unsetThemeSustain() {
+        document.body.classList.remove('theme-sustain');
+        localStorage.removeItem('theme-sustain');
+        document.querySelector('.lv-hero > .lv-hero-item:first-child').style.backgroundImage = 'url(/assets/img/content/hero-1.jpg)';
+        document.querySelector('.lv-hero-caption .panel-feature h1').innerHTML = 'Engineers for Building Services';
+        document.querySelector('.lv-hero-caption .panel-feature p').innerHTML = "erbas™ is a multi-disciplinary consulting engineering firm serving clients in Commercial, Health, Education, Government, Industrial and Sustainability sectors.";
+    }
+
+} else {
+    localStorage.removeItem('theme-sustain');
+}
 
 //-----------------------------------------------------------------
 // POPUP GALLERY
